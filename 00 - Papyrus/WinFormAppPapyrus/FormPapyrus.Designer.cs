cfcf9b8f77f0e24150b239ec59b407ffd713720e
kyweez <auboudier.pro@gmail.com>
2020-12-10 22:17:59 +0100

    Papyrus / HMI ok

--- 00 - Papyrus/WinFormAppPapyrus/FormPapyrus.Designer.cs
+++ 00 - Papyrus/WinFormAppPapyrus/FormPapyrus.Designer.cs
@@ -29,13 +29,138 @@ namespace WinFormAppPapyrus
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.textBoxDatabase = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.labelServer = new System.Windows.Forms.Label();
+            this.labelDatabase = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(132, 35);
+            this.textBoxServer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(298, 24);
+            this.textBoxServer.TabIndex = 0;
+            // 
+            // textBoxDatabase
+            // 
+            this.textBoxDatabase.Location = new System.Drawing.Point(132, 97);
+            this.textBoxDatabase.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxDatabase.Name = "textBoxDatabase";
+            this.textBoxDatabase.Size = new System.Drawing.Size(298, 24);
+            this.textBoxDatabase.TabIndex = 1;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(132, 153);
+            this.buttonConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(112, 32);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(318, 153);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(318, 518);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 32);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Quit";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(42, 259);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(388, 229);
+            this.textBox3.TabIndex = 5;
+            // 
+            // labelServer
+            // 
+            this.labelServer.AutoSize = true;
+            this.labelServer.Location = new System.Drawing.Point(39, 35);
+            this.labelServer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelServer.Name = "labelServer";
+            this.labelServer.Size = new System.Drawing.Size(51, 18);
+            this.labelServer.TabIndex = 6;
+            this.labelServer.Text = "Server";
+            // 
+            // labelDatabase
+            // 
+            this.labelDatabase.AutoSize = true;
+            this.labelDatabase.Location = new System.Drawing.Point(39, 97);
+            this.labelDatabase.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDatabase.Name = "labelDatabase";
+            this.labelDatabase.Size = new System.Drawing.Size(71, 18);
+            this.labelDatabase.TabIndex = 7;
+            this.labelDatabase.Text = "Database";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(39, 227);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(187, 18);
+            this.labelStatus.TabIndex = 8;
+            this.labelStatus.Text = "Connection status : Closed";
+            // 
+            // FormPapyrus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(477, 567);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelDatabase);
+            this.Controls.Add(this.labelServer);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxDatabase);
+            this.Controls.Add(this.textBoxServer);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "FormPapyrus";
+            this.Text = "Database connection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.TextBox textBoxDatabase;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label labelServer;
+        private System.Windows.Forms.Label labelDatabase;
+        private System.Windows.Forms.Label labelStatus;
     }
 }
 
